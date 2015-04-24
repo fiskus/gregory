ee161d4c187853a7e0af0b67fcb282872e1ed10d
Maxim Chervonny <fiskus@chervonny.ru>
2015-04-24 23:52:34 +0300

    Dzisus, kurwa, ja pierdole!

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var fs = require('fs');
+var stylus = require('gulp-stylus');
+var browserify = require('browserify');
+var jshint = require('gulp-jshint');
+var karma = require('karma').server;
+
+gulp.task('stylus', function() {
+    gulp.src('./styl/app.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('watch-stylus', function () {
+    gulp.watch('./styl/*.styl',
+            ['stylus']);
+});
+
+gulp.task('js', function () {
+    browserify('./examples/basic/main.js')
+        .bundle()
+        .pipe(fs.createWriteStream('./examples/basic/bundle.js'));
+});
+
+gulp.task('watch-js', function () {
+    gulp.watch(['./lib/*.js', './examples/basic/main.js'],
+               ['js']);
+});
+
+gulp.task('watch', ['watch-stylus', 'watch-js']);
+
+gulp.task('dev', ['stylus', 'js', 'watch']);
+
+gulp.task('lint', function () {
+    gulp.src('./lib/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('test', function (done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done);
+});
+
+gulp.task('tdd', function (done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.js'
+    }, done);
+});
+
+gulp.task('default', ['stylus', 'js']);
