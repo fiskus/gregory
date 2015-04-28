c3a17cfc9ac7864e99fb160f564313d00f2075e2
Maxim Chervonny <fiskus@chervonny.ru>
2015-04-28 15:04:11 +0300

    option for setting default date

--- examples/basic/main.js
+++ examples/basic/main.js
@@ -8,14 +8,15 @@ var Calendar = require('../../lib/index.js');
 require('moment/locale/nb');
 
 function onDatePicked(date) {
-    console.log(date);
+    console.log(date, date.format('YYYY-MM-DD'));
 }
 
 var wrapper = document.getElementById('calendar');
 var calendarSettings = {
+    defaultDate: new Date('2015-02-12'),
     onPickDate: onDatePicked,
-    showDaysOfWeek: true
-    // forceSixRows: true
+    showDaysOfWeek: true,
+    forceSixRows: true
     // rootClassName: 'calendar'
 };
 var calendarElement = React.createElement(Calendar, calendarSettings);
