4f2b4cd62b0d213815f69411151ff368444ef858
Maxim Chervonny <fiskus@chervonny.ru>
2015-04-30 19:55:41 +0300

    add inputs to select calendar

--- examples/basic/main.js
+++ examples/basic/main.js
@@ -12,47 +12,91 @@ function onDatePicked(date) {
     console.log(date, date.format('YYYY-MM-DD'));
 }
 
-var clndrWrapper = document.getElementById('clndr');
-var clndrSettings = {
-    DEFAULT_DATE: new Date('2015-02-12'),
-    FORCE_SIX_ROWS: false,
-    MONTH_FORMAT: 'MMM YYYY',
-    NEXT_TEXT: 'След',
-    ON_PICK_DATE: onDatePicked,
-    PREV_TEXT: 'Пред',
-    SHOW_WEEKDAYS: true,
-    WEEKDAYS: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
-};
-var clndrElement = React.createElement(Calendar, clndrSettings);
-React.render(clndrElement, clndrWrapper);
+function initSingleMonthUglyCalendar () {
+    var wrapper = document.getElementById('clndr');
+    var settings = {
+        DEFAULT_DATE: new Date('2015-02-12'),
+        FORCE_SIX_ROWS: false,
+        MONTH_FORMAT: 'MMM YYYY',
+        NEXT_TEXT: 'След',
+        ON_PICK_DATE: onDatePicked,
+        PREV_TEXT: 'Пред',
+        SHOW_WEEKDAYS: true,
+        WEEKDAYS: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+    };
+    var element = React.createElement(Calendar, settings);
+    React.render(element, wrapper);
+}
+
+function initTrilpeMonthCalendar () {
+    var wrapper = document.getElementById('calendar');
+    var settings = {
+        DEFAULT_DATE: new Date('2015-05-12'),
+        FORCE_SIX_ROWS: false,
+        MONTH_FORMAT: 'MMMM YYYY',
+        NEXT_TEXT: '',
+        NUMBER_OF_MONTHS: 3,
+        ON_PICK_DATE: onDatePicked,
+        PREV_TEXT: '',
+        ROOT_CLASSNAME: 'calendar',
+        SHOW_WEEKDAYS: true,
+        WEEKDAYS: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
+        MIN_DATE: new Date('2015-06-10'),
+        MAX_DATE: new Date('2015-07-10'),
+        SELECTS: [{
+            DATE: new Date('2015-06-14'),
+            CLASSNAME: 'day-checkin'
+        }, {
+            DATE: new Date('2015-06-18'),
+            CLASSNAME: 'day-checkout'
+        }],
+        RANGES: [{
+            FROM: new Date('2015-06-19'),
+            TO: new Date('2015-06-24'),
+            CLASSNAME: 'day-range'
+        }]
+    };
+    var element = React.createElement(Calendar, settings);
+    React.render(element, wrapper);
+}
+
+function initPopup (input, wrapper) {
+    input.addEventListener('focus', function() {
+        var element = React.createElement(Calendar, {
+            ROOT_CLASSNAME: 'calendar',
+            PREV_TEXT: '',
+            NEXT_TEXT: '',
+            ON_PICK_DATE: function(date) {
+                wrapper.removeChild(wrapper.firstElementChild);
+                input.value = date.format('DD MMMM YYYY');
+            }
+        });
+        React.render(element, wrapper);
+    });
+}
+
+function initInputs () {
+    var inputsWrapper = document.getElementById('inputs');
+    var checkinInput = inputsWrapper.querySelector('#checkin');
+    var checkoutInput = inputsWrapper.querySelector('#checkout');
+
+    var checkinCalendar = inputsWrapper.querySelector('#checkin-calendar');
+    var checkoutCalendar = inputsWrapper.querySelector('#checkout-calendar');
+
+    initPopup(checkinInput, checkinCalendar);
+    initPopup(checkoutInput, checkoutCalendar);
+
+}
+
+function whenReady () {
+    initSingleMonthUglyCalendar();
+    initTrilpeMonthCalendar();
+    initInputs();
+}
 
 
-var calendarWrapper = document.getElementById('calendar');
-var calendarSettings = {
-    DEFAULT_DATE: new Date('2015-05-12'),
-    FORCE_SIX_ROWS: false,
-    MONTH_FORMAT: 'MMMM YYYY',
-    NEXT_TEXT: '',
-    NUMBER_OF_MONTHS: 3,
-    ON_PICK_DATE: onDatePicked,
-    PREV_TEXT: '',
-    ROOT_CLASSNAME: 'calendar',
-    SHOW_WEEKDAYS: true,
-    WEEKDAYS: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
-    MIN_DATE: new Date('2015-06-10'),
-    MAX_DATE: new Date('2015-07-10'),
-    SELECTS: [{
-        DATE: new Date('2015-06-14'),
-        CLASSNAME: 'day-checkin'
-    }, {
-        DATE: new Date('2015-06-18'),
-        CLASSNAME: 'day-checkout'
-    }],
-    RANGES: [{
-        FROM: new Date('2015-06-19'),
-        TO: new Date('2015-06-24'),
-        CLASSNAME: 'day-range'
-    }]
-};
-var calendarElement = React.createElement(Calendar, calendarSettings);
-React.render(calendarElement, calendarWrapper);
+if (document.readyState !== 'loading') {
+    whenReady();
+} else {
+    window.addEventListener('DOMContentLoaded', whenReady);
+}
