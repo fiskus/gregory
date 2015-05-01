2ddd421ee7674c4bf7f277dcdf7c1067f8983b9c
Maxim Chervonny <fiskus@chervonny.ru>
2015-05-02 02:10:00 +0300

    cover all functions in helpers by tests

--- specs/helpers.spec.js
+++ specs/helpers.spec.js
@@ -5,16 +5,12 @@ require('../lib/bindshim.js');
 
 var H = require('../lib/helpers.js');
 
-// TODO:
-// forEachInArray
-// assignObject
-// forEachInObject
-
 describe('Helpers', function () {
     it('mapArray', function() {
-        expect(H.mapArray([1, 2, 3, 4], function(n) {
+        function multiple2 (n) {
             return n * 2;
-        })).toEqual([2, 4, 6, 8]);
+        }
+        expect(H.mapArray([1, 2, 3, 4], multiple2)).toEqual([2, 4, 6, 8]);
     });
 
     it('getClassName', function() {
@@ -36,5 +32,37 @@ describe('Helpers', function () {
         expect(H.getRange(1)).toEqual([0, 1]);
         expect(H.getRange(3)).toEqual([0, 1, 2, 3]);
         expect(H.getRange(3, 6)).toEqual([3, 4, 5, 6]);
+    });
+
+    it('forEachInArray', function() {
+        var output = 0;
+        function add (n) {
+            output += n;
+        }
+        H.forEachInArray([1, 2, 3, 4], add);
+        expect(output).toEqual(10);
+    });
+
+    it('assignObject', function() {
+        expect(H.assignObject({a: 1}, {b: 2}, {c: 3})).toEqual({
+            a: 1,
+            b: 2,
+            c: 3
+        });
+    });
+
+    it('forEachInObject', function() {
+        var obj = {
+            a: 1,
+            b: 2,
+            c: 3,
+            d: 4
+        };
+        var outputs = [];
+        function stringify (value, key) {
+            outputs.push(key + ':' + value);
+        }
+        H.forEachInObject(obj, stringify);
+        expect(outputs.join()).toBe('a:1,b:2,c:3,d:4');
     });
 });
