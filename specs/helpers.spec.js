11027a9eae4d1ecb642db0bea0910a0b721eb6c6
Maxim Chervonny <fiskus@chervonny.ru>
2015-10-20 15:11:05 +0300

    remove abstracted helpers for native es5 methods

--- specs/helpers.spec.js
+++ specs/helpers.spec.js
@@ -8,14 +8,7 @@ require('../lib/bindshim.js');
 var H = require('../lib/helpers.js');
 
 describe('Helpers', function () {
-    it('mapArray', function() {
-        function multiple2 (n) {
-            return n * 2;
-        }
-        expect(H.mapArray([1, 2, 3, 4], multiple2)).toEqual([2, 4, 6, 8]);
-    });
-
-    it('getClassName', function() {
+    it('getClassName', function () {
         expect(H.getClassName({
             CLASSNAME: 'clndr'
         })).toBe('clndr');
@@ -29,23 +22,14 @@ describe('Helpers', function () {
         }, ['kekeke', 'ololo'])).toBe('clndr-kekeke clndr-ololo');
     });
 
-    it('getRange', function() {
+    it('getRange', function () {
         expect(H.getRange()).toEqual([0]);
         expect(H.getRange(1)).toEqual([0, 1]);
         expect(H.getRange(3)).toEqual([0, 1, 2, 3]);
         expect(H.getRange(3, 6)).toEqual([3, 4, 5, 6]);
     });
 
-    it('forEachInArray', function() {
-        var output = 0;
-        function add (n) {
-            output += n;
-        }
-        H.forEachInArray([1, 2, 3, 4], add);
-        expect(output).toEqual(10);
-    });
-
-    it('assignObject', function() {
+    it('assignObject', function () {
         // jscs:disable requirePaddingNewLinesInObjects
         expect(H.assignObject({a: 1}, {b: 2}, {c: 3})).toEqual({
             a: 1,
@@ -54,7 +38,7 @@ describe('Helpers', function () {
         });
     });
 
-    it('forEachInObject', function() {
+    it('forEachInObject', function () {
         var obj = {
             a: 1,
             b: 2,
